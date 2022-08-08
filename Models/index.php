49fd46c8c5e997f687913b4a1e37419d9d431e52
Maram <107946222+Maram-Alsharif@users.noreply.github.com>
2022-08-08 23:36:24 +0300

    Delete Models directory

--- Models/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_unset();// هنا سوينا اوبجيكت من كلاس الكنترولر واستدعينا الميثود عشان يحدد نوع العملية 
-	require_once  'StudentController.php';		
-    $controller = new Controller();	
-    $controller->mvcHandler();
-?>
